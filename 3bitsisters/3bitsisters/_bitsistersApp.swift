f2e055b27103dad36e98f39352e1924898f7cfec
vinceline <bertrandvinceline@gmail.com>
2025-09-19 19:35:05 -0400

    initialize swiftui project

---
+++ 3bitsisters/3bitsisters/_bitsistersApp.swift
@@ -0,0 +1,17 @@
+//
+//  _bitsistersApp.swift
+//  3bitsisters
+//
+//  Created by Vinceline Bertrand on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct _bitsistersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 3bitsisters/3bitsistersTests/_bitsistersTests.swift
@@ -0,0 +1,17 @@
+//
+//  _bitsistersTests.swift
+//  3bitsistersTests
+//
+//  Created by Vinceline Bertrand on 9/19/25.
+//
+
+import Testing
+@testable import _bitsisters
+
+struct _bitsistersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
